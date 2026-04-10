19892c867283a397341ad5600dfa3ab1f6b23ce0
Andreas Fitzek <andreas.fitzek@n8n.io>
2026-04-10 15:04:39 +0200

    Make rate limits in token exchange configurable

--- packages/cli/src/modules/token-exchange/controllers/embed-auth.controller.ts
+++ packages/cli/src/modules/token-exchange/controllers/embed-auth.controller.ts
@@ -8,6 +8,10 @@ import { AuthlessRequest } from '@/requests';
 import { UrlService } from '@/services/url.service';
 
 import { TokenExchangeService } from '../services/token-exchange.service';
+import { TokenExchangeConfig } from '../token-exchange.config';
+import { Container } from '@n8n/di';
+
+const configService = Container.get(TokenExchangeConfig);
 
 @RestController('/auth/embed')
 export class EmbedAuthController {
@@ -19,7 +23,10 @@ export class EmbedAuthController {
 
 	@Get('/', {
 		skipAuth: true,
-		ipRateLimit: { limit: 20, windowMs: 1 * Time.minutes.toMilliseconds },
+		ipRateLimit: {
+			limit: configService.rateLimitEmbedLogin,
+			windowMs: 1 * Time.minutes.toMilliseconds,
+		},
 	})
 	async getLogin(req: AuthlessRequest, res: Response, @Query query: EmbedLoginQueryDto) {
 		return await this.handleLogin(query.token, req, res);
@@ -27,7 +34,10 @@ export class EmbedAuthController {
 
 	@Post('/', {
 		skipAuth: true,
-		ipRateLimit: { limit: 20, windowMs: 1 * Time.minutes.toMilliseconds },
+		ipRateLimit: {
+			limit: configService.rateLimitEmbedLogin,
+			windowMs: 1 * Time.minutes.toMilliseconds,
+		},
 	})
 	async postLogin(req: AuthlessRequest, res: Response, @Body body: EmbedLoginBodyDto) {
 		return await this.handleLogin(body.token, req, res);
